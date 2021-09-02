04a14cd4aa35b88970ac6917f30a81b8c625ce00
test <test@test.com>
2021-09-02 12:36:14 +0800

    code format fixed

--- app.py
+++ app.py
@@ -55,21 +55,48 @@ def show(event):
                 line_bot_api.reply_message(
                     event.reply_token,
                     TextSendMessage(
-                        text='查無此本: ' + event.message.text + '。\n請確認輸入是否正確，如果要隨機產生本子，輸入-1即可')
+                        text=(
+                            f'查無此本: {event.message.text}。\n'
+                            '請確認輸入是否正確，如果要隨機產生本子，輸入-1即可'
+                        )
+                    )
                 )
             else:
-                reply_arr.append(TextSendMessage(
-                    text='https://nhentai.net/g/' + hentai.name + '/'))
+                reply_arr.append(
+                    TextSendMessage(
+                        text='https://nhentai.net/g/' + hentai.name + '/'
+                    )
+                )
 
                 res = hentai.getInfo()
 
-                reply_arr.append(ImageSendMessage(
-                    original_content_url=res[0],
-                    preview_image_url=res[0]))
-                reply_arr.append(TextSendMessage(
-                    text=f'***若欄位為空白，表示網站亦無該資訊***\n\n主標題(Main Title):\n{res[1]}\n\n副標題(Sub Title):\n{res[2]}\n\n原作(Parodies):\n{res[3]}\n\n角色(Characters):\n{res[4]}\n\n標籤(Tags):\n{res[5]}\n\n作者(Artists):\n{res[6]}\n\n語言(Languages):\n{res[7]}\n\n本子類型(Catogories):\n{res[8]}\n\n頁數(Pages):\n{res[9]}'))
-                reply_arr.append(TextSendMessage(
-                    text='若需繼續查詢，請直接輸入文字或號碼，輸入-1可隨機搜尋本子。'))
+                reply_arr.append(
+                    ImageSendMessage(
+                        original_content_url=res[0],
+                        preview_image_url=res[0]
+                    )
+                )
+                reply_arr.append(
+                    TextSendMessage(
+                        text=(
+                            '***若欄位為空白，表示網站亦無該資訊***\n\n'
+                            f'主標題(Main Title):\n{res[1]}\n\n'
+                            f'副標題(Sub Title):\n{res[2]}\n\n'
+                            f'原作(Parodies):\n{res[3]}\n\n'
+                            f'角色(Characters):\n{res[4]}\n\n'
+                            f'標籤(Tags):\n{res[5]}\n\n'
+                            f'作者(Artists):\n{res[6]}\n\n'
+                            f'語言(Languages):\n{res[7]}\n\n'
+                            f'本子類型(Catogories):\n{res[8]}\n\n'
+                            f'頁數(Pages):\n{res[9]}'
+                        )
+                    )
+                )
+                reply_arr.append(
+                    TextSendMessage(
+                        text='若需繼續查詢，請直接輸入文字或號碼，輸入-1可隨機搜尋本子。'
+                    )
+                )
                 line_bot_api.reply_message(
                     event.reply_token,
                     reply_arr
@@ -84,7 +111,11 @@ def show(event):
             line_bot_api.reply_message(
                 event.reply_token,
                 TextSendMessage(
-                    text='查無此標籤，亦無此本子: ' + event.message.text + '。\n請確認輸入是否正確，並重新輸入，如果要隨機產生本子，輸入-1即可')
+                    text=(
+                        f'查無此標籤，亦無此本子: {event.message.text}。\n'
+                        '請確認輸入是否正確，並重新輸入，如果要隨機產生本子，輸入-1即可'
+                    )
+                )
             )
         else:
             reply_arr.append(TextSendMessage(
@@ -100,16 +131,54 @@ def show(event):
     if event.message.text == '使用說明':
         reply_arr = []
 
-        reply_arr.append(TextSendMessage(
-            text='歡迎使用nhentai爬蟲機器人\n根據輸入為文字或數字，會自動搜尋標籤或本子\n\n更新紀錄可以透過輸入「更新紀錄」查看'))
-        reply_arr.append(TextSendMessage(
-            text='關鍵字搜尋:\n若輸入文字，會搜尋與該文字相關之本子，並列出至多25個今日熱門的結果。\n\n範例:\n輸入「paizuri」，會列出25本與「paizuri」相關之今日最熱門的本子\n\n支援搜尋多個標籤，只需以空格分隔各個標籤即可，亦支援限定語言。\n\n範例:\n輸入「paizuri stockings chinese」，會列出25本與「paizuri」與「stockings」相關，語言為「中文」的本子\n\n備註：目前尚無法以中文搜尋'))
-        reply_arr.append(TextSendMessage(
-            text='本子搜尋:\n若輸入數字，會搜尋號碼為該數字的本子，並列出與其相關的資訊。\n\n範例:\n輸入「335974」，會搜尋335974這個本子，並列出網址、封面圖片、標題、原作、角色、標籤、作者、語言、類型，頁數等資訊'))
-        reply_arr.append(TextSendMessage(
-            text='☆全新功能☆\n\n本月推薦:\n顧名思義，輸入「本月推薦」，會列出5~15本我個人推薦的本子，僅列出網址，部分標籤及頁數，每月10號更新一次。\n\n備註：因為我記憶力不是很好，所以如果發現沒有更新，可以透過點擊「建議與反饋」，從聯絡資訊告知我'))
-        reply_arr.append(TextSendMessage(
-            text='隨機搜尋:\n若輸入-1，會隨機搜尋一個本子，並列出其相關資訊。\n\n備註:目前尚未實裝篩選系統，尚無法依條件篩選隨機之結果'))
+        reply_arr.append(
+            TextSendMessage(
+                text=(
+                    '歡迎使用nhentai爬蟲機器人\n'
+                    '根據輸入為文字或數字，會自動搜尋標籤或本子\n\n'
+                    '更新紀錄可以透過輸入「更新紀錄」查看'
+                )
+            )
+        )
+        reply_arr.append(
+            TextSendMessage(
+                text=(
+                    '關鍵字搜尋:\n'
+                    '若輸入文字，會搜尋與該文字相關之本子，並列出至多25個今日熱門的結果。\n\n'
+                    '範例:\n輸入「paizuri」，會列出25本與「paizuri」相關之今日最熱門的本子\n\n'
+                    '支援搜尋多個標籤，只需以空格分隔各個標籤即可，亦支援限定語言。\n\n'
+                    '範例:\n輸入「paizuri stockings chinese」，會列出25本與「paizuri」與「stockings」相關，語言為「中文」的本子\n\n'
+                    '備註：目前尚無法以中文搜尋'
+                )
+            )
+        )
+        reply_arr.append(
+            TextSendMessage(
+                text=(
+                    '本子搜尋:\n'
+                    '若輸入數字，會搜尋號碼為該數字的本子，並列出與其相關的資訊。\n\n'
+                    '範例:\n輸入「335974」，會搜尋335974這個本子，並列出網址、封面圖片、標題、原作、角色、標籤、作者、語言、類型，頁數等資訊'
+                )
+            )
+        )
+        reply_arr.append(
+            TextSendMessage(
+                text=(
+                    '本月推薦:\n'
+                    '顧名思義，輸入「本月推薦」，會列出5~15本我個人推薦的本子，僅列出網址，部分標籤及頁數，每月10號更新一次。\n\n'
+                    '備註：因為我記憶力不是很好，所以如果發現沒有更新，可以透過點擊「建議與反饋」，從聯絡資訊告知我'
+                )
+            )
+        )
+        reply_arr.append(
+            TextSendMessage(
+                text=(
+                    '隨機搜尋:\n'
+                    '若輸入-1，會隨機搜尋一個本子，並列出其相關資訊。\n\n'
+                    '備註:目前尚未實裝篩選系統，尚無法依條件篩選隨機之結果'
+                )
+            )
+        )
 
         line_bot_api.reply_message(
             event.reply_token,
@@ -119,10 +188,21 @@ def show(event):
     if event.message.text == '建議':
         reply_arr = []
 
-        reply_arr.append(TextSendMessage(
-            text='感謝您使用這個機器人，若有任何建議或回饋，可透過下方資訊聯絡我。'))
-        reply_arr.append(TextSendMessage(
-            text='聯絡資訊:\n\nDiscord: Lazy#3082\nInstagram: i_am_lazy_boy_/diligent_meme\nmail: machael1209@gmail.com'))
+        reply_arr.append(
+            TextSendMessage(
+                text='感謝您使用這個機器人，若有任何建議或回饋，可透過下方資訊聯絡我。'
+            )
+        )
+        reply_arr.append(
+            TextSendMessage(
+                text=(
+                    '聯絡資訊:\n\n'
+                    'Discord: Lazy#3082\n'
+                    'Instagram: i_am_lazy_boy_\n'
+                    'mail: machael1209@gmail.com'
+                )
+            )
+        )
 
         line_bot_api.reply_message(
             event.reply_token,
@@ -132,15 +212,19 @@ def show(event):
     if event.message.text == '本月推薦':
         reply_arr = []
 
-        reply_arr.append(TextSendMessage(
-            text='\
-https://nhentai.net/g/369484/\n巨乳 女學生制服 中出\n98頁\n\n\
-https://nhentai.net/g/368990/\n只有一個女性 只有一個男性 絲襪\n24頁\n\n\
-https://nhentai.net/g/261701/\n絲襪 乳交 女性主導 女僕\n18頁\n\n\
-https://nhentai.net/g/235772/\n只有一個女性 只有一個男性\n20頁\n\n\
-https://nhentai.net/g/369043/\n絲襪 女僕\n20頁\n\n\
-https://nhentai.net/g/369407/\n辣妹 女學生制服 女性主導\n20頁\n\n\
-https://nhentai.net/g/359173/\n群交 只有一個男性\n76頁\n\n'))
+        reply_arr.append(
+            TextSendMessage(
+                text=(
+                    'https://nhentai.net/g/369484/\n巨乳 女學生制服 中出\n98頁\n\n'
+                    'https://nhentai.net/g/368990/\n只有一個女性 只有一個男性 絲襪\n24頁\n\n'
+                    'https://nhentai.net/g/261701/\n絲襪 乳交 女性主導 女僕\n18頁\n\n'
+                    'https://nhentai.net/g/235772/\n只有一個女性 只有一個男性\n20頁\n\n'
+                    'https://nhentai.net/g/369043/\n絲襪 女僕\n20頁\n\n'
+                    'https://nhentai.net/g/369407/\n辣妹 女學生制服 女性主導\n20頁\n\n'
+                    'https://nhentai.net/g/359173/\n群交 只有一個男性\n76頁\n\n'
+                )
+            )
+        )
 
         line_bot_api.reply_message(
             event.reply_token,
@@ -150,10 +234,15 @@ https://nhentai.net/g/359173/\n群交 只有一個男性\n76頁\n\n'))
     if event.message.text == '更新紀錄':
         reply_arr = []
 
-        reply_arr.append(TextSendMessage(
-            text='更新紀錄：\n\n\
-2021/08/16 18:00\n1.新增「本月推薦」功能\n2.新增「更新紀錄」功能\n3.修改「使用說明」部分內容\n\n\
-2021/08/19 12:00\n1.修改「使用說明」部分內容'))
+        reply_arr.append(
+            TextSendMessage(
+                text=(
+                    '更新紀錄：\n\n'
+                    '2021/08/16 18:00\n1.新增「本月推薦」功能\n2.新增「更新紀錄」功能\n3.修改「使用說明」部分內容\n\n'
+                    '2021/08/19 12:00\n1.修改「使用說明」部分內容'
+                )
+            )
+        )
 
         line_bot_api.reply_message(
             event.reply_token,
